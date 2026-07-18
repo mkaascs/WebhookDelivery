41c4895d0e987175622791752db6a2188586703a
Maxim <teakeyx@gmail.com>
2026-07-18 03:25:54 +0300

    added crypto and uuid libs

--- internal/lib/crypto/secret.go
+++ internal/lib/crypto/secret.go
@@ -1,1 +1,15 @@
 package crypto
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+const secretSize = 32
+
+func NewSecret() string {
+	bytes := make([]byte, secretSize)
+	_, _ = rand.Read(bytes)
+	return fmt.Sprintf("whsec_%s", base64.StdEncoding.EncodeToString(bytes))
+}
--- internal/lib/uuid/uuid.go
+++ internal/lib/uuid/uuid.go
@@ -1,1 +1,32 @@
 package uuid
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+const idLength = 16
+
+func New(prefix string) string {
+	bytes := make([]byte, idLength)
+	_, _ = rand.Read(bytes)
+
+	return fmt.Sprintf("%s_%s", prefix, base64.URLEncoding.EncodeToString(bytes))
+}
+
+func NewEndpoint() string {
+	return New("ep")
+}
+
+func NewDelivery() string {
+	return New("del")
+}
+
+func NewSubscription() string {
+	return New("sub")
+}
+
+func NewEvent() string {
+	return New("ev")
+}
