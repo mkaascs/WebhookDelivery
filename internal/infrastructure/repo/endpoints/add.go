a5a137715b5ad285fdb633d1a6e8d2096e88ed3e
Maxim <teakeyx@gmail.com>
2026-07-18 03:41:20 +0300

    implemented add endpoint repo func

--- internal/infrastructure/repo/endpoints/add.go
+++ internal/infrastructure/repo/endpoints/add.go
@@ -2,8 +2,10 @@ package endpoints
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log/slog"
+	"time"
 	"webhook-delivery/internal/domain/dto"
 	sloglib "webhook-delivery/internal/lib/logging/slog"
 	"webhook-delivery/internal/lib/uuid"
@@ -19,20 +21,51 @@ func (r *Repo) Add(ctx context.Context, command dto.AddEndpointCommand) (*dto.Ad
 	}
 
 	success := false
-	defer func() {
-		if success {
-			return
+	defer func(tx *sql.Tx) {
+		if !success {
+			if err := tx.Rollback(); err != nil {
+				log.Error("failed to rollback tx", sloglib.Error(err))
+			}
 		}
+	}(tx)
 
-		if err := tx.Rollback(); err != nil {
-			log.Error("failed to rollback tx", sloglib.Error(err))
-		}
-	}()
+	var endpointID string
+	var createdAt time.Time
 
-	subsID := make([]string, 0, len(command.EventTypes))
-	for count := 0; count < len(command.EventTypes); count++ {
-		subsID = append(subsID, uuid.NewSubscription())
+	err = tx.QueryRow(`
+		INSERT INTO endpoints (id, url, secret, description, is_active)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id, created_at`).
+		Scan(&endpointID, &createdAt)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", fn, err)
 	}
 
-	return nil, nil
+	stmt, err := tx.PrepareContext(ctx, `
+		INSERT INTO subscriptions (id, endpoint_id, event_type)
+		VALUES ($1, $2, $3)`)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", fn, err)
+	}
+
+	defer func(stmt *sql.Stmt) {
+		if err := stmt.Close(); err != nil {
+			log.Warn("failed to close statement", sloglib.Error(err))
+		}
+	}(stmt)
+
+	for _, eventType := range command.EventTypes {
+		_, err := stmt.ExecContext(ctx, uuid.NewSubscription(), endpointID, eventType)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", fn, err)
+		}
+	}
+
+	success = true
+	return &dto.AddEndpointResult{
+		ID:        endpointID,
+		CreatedAt: createdAt,
+	}, nil
 }
